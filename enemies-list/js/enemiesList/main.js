55316661b5c9fb45a666bebd1e4d97c75728a70b
Joel Beckum <joelbeckum@gmail.com>
2021-08-23 14:28:42 -0400

    Completed Enemies List assignment

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,64 @@
-// Put your code here
+const createEnemy = (firstName, lastName, offenses, isReallyHated) => {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        offenses: offenses,
+        isReallyHated: isReallyHated
+    }
+}
+
+const getEnemies = () => {
+    const enemies = [createEnemy(
+        "Joshua",
+        "Flowers",
+        [
+            "Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"
+        ],
+        true
+    ),
+        createEnemy(
+            "Darth",
+            "Vader",
+            [
+                "Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"
+            ],
+            false
+        ),
+        createEnemy(
+            "Betty",
+            "Rudelady",
+            [
+                "Phone calls in the theater",
+                "Phone calls on the bus",
+                "Phone calls in line at the grocery store",
+                "Poor conversationalist"
+            ],
+            true
+        ),
+        createEnemy(
+            "Leon",
+            "Peck",
+            [
+                "Keeps giving me a hotplate"
+            ],
+            false
+        )]
+    
+    return enemies;
+}
+
+console.log("My Enemies List!");
+console.log("----------------");
+
+const enemies = getEnemies();
+
+for (const enemy of enemies) {
+    if (enemy.isReallyHated) {
+        console.log(`${enemy.firstName} ${enemy.lastName} (Really, really dislike!)`);
+    } else {
+        console.log(`${enemy.firstName} ${enemy.lastName}`);
+    }
+}
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,17 @@
-// Put your code here
-
 const names = [
    "Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+const theNames = names.filter(name => name.startsWith("The"));
+
+console.log("\n'The' Place Names\n");
+for (const name of theNames) {
   console.log(name);
 }
